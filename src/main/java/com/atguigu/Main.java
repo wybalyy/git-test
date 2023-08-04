b950327bf0d9b61315c723a958b3810cffc68020
Lyy <Lyy@qq.com>
2023-08-04 09:57:42 +0800

    hot-fix commit

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         System.out.println("Hello git");
+        System.out.println("Hello git1");
 
 
     }
